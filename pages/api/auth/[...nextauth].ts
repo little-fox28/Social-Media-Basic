f208557945c05a7b8c5e19556959acc9d2d2b453
Little-Fox28 <tranthjnh28@gmail.com>
2023-03-16 17:59:48 +0700

    authentication with Prisma, MongoDB, NextAuth 50% laoding...

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,52 @@
+import nextAuth, { Awaitable, RequestInternal, User } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import bcrypt from 'bcrypt';
+
+import prisma from '@/libs/prismadb';
+
+export default nextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error('Invalid credential');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid credential');
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error('Invalid credential');
+        }
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
