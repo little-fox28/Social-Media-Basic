00cd2a3aea17e88ed57737c87fd8a37bf43c7c74
Little-Fox28 <tranthjnh28@gmail.com>
2023-03-24 03:01:45 +0700

    handle follow bar

--- components/hooks/useUsers.ts
+++ components/hooks/useUsers.ts
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 import fetcher from '@/libs/fetcher';
 
 const useUSers = () => {
-  const { data, error, isLoading, mutate } = useSWR('/api/user', fetcher);
+  const { data, error, isLoading, mutate } = useSWR('/api/users', fetcher);
 
   return {
     data,
